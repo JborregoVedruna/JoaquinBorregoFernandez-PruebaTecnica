b5ae437aae7828553b497a92d810189cec20f0c2
Joaquin <joaquin.borrego@vedruna.es>
2026-02-03 02:56:42 +0100

    problema de cors

--- src/main/java/com/caixabank/loansmanager/config/SecurityConfig.java
+++ src/main/java/com/caixabank/loansmanager/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,16 +23,19 @@ public class SecurityConfig {
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   /**
-   * Proveedor de autenticación configurado para la carga de usuarios y codificación de contraseñas
+   * Proveedor de autenticación configurado para la carga de usuarios y
+   * codificación de contraseñas
    * (definido en ApplicationConfig).
    */
   private final AuthenticationProvider authProvider;
 
   /**
-   * Define la cadena de filtros de seguridad (SecurityFilterChain) que interceptará todas las
+   * Define la cadena de filtros de seguridad (SecurityFilterChain) que
+   * interceptará todas las
    * peticiones HTTP.
    *
-   * <p>Esta es la configuración central de la seguridad de la aplicación.
+   * <p>
+   * Esta es la configuración central de la seguridad de la aplicación.
    *
    * @param http Objeto para configurar Spring Security a nivel HTTP.
    * @return La cadena de filtros de seguridad construida.
@@ -43,33 +47,33 @@ public class SecurityConfig {
         // 1. Deshabilita la protección CSRF (CSRF es innecesario para APIs REST sin
         // sesiones)
         .csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults())
         // 2. Configura las reglas de autorización para las peticiones HTTP
         .authorizeHttpRequests(
-            authReq ->
-                authReq
-                    // Permite acceso sin autenticación a endpoints públicos y de
-                    // documentación
-                    .requestMatchers("/v3/api-docs/**")
-                    .permitAll() // OpenAPI/Swagger Docs
-                    .requestMatchers("/swagger-ui/**")
-                    .permitAll() // Swagger
-                    .requestMatchers("/swagger-ui.html")
-                    .permitAll() // Swagger
-                    .requestMatchers("/error")
-                    .permitAll() // Error
-                    .requestMatchers("/h2-console/**")
-                    .permitAll() // H2 Console
-                    .requestMatchers("/api/v1/auth/register")
-                    .permitAll()
-                    .requestMatchers("/api/v1/auth/login")
-                    .permitAll()
-                    .requestMatchers("/actuator/**")
-                    .permitAll()
-                    .requestMatchers("/public/**")
-                    .permitAll()
-                    // Cualquier otra petición requiere autenticación
-                    .anyRequest()
-                    .authenticated())
+            authReq -> authReq
+                // Permite acceso sin autenticación a endpoints públicos y de
+                // documentación
+                .requestMatchers("/v3/api-docs/**")
+                .permitAll() // OpenAPI/Swagger Docs
+                .requestMatchers("/swagger-ui/**")
+                .permitAll() // Swagger
+                .requestMatchers("/swagger-ui.html")
+                .permitAll() // Swagger
+                .requestMatchers("/error")
+                .permitAll() // Error
+                .requestMatchers("/h2-console/**")
+                .permitAll() // H2 Console
+                .requestMatchers("/api/v1/auth/register")
+                .permitAll()
+                .requestMatchers("/api/v1/auth/login")
+                .permitAll()
+                .requestMatchers("/actuator/**")
+                .permitAll()
+                .requestMatchers("/public/**")
+                .permitAll()
+                // Cualquier otra petición requiere autenticación
+                .anyRequest()
+                .authenticated())
         // 3. Configura la gestión de sesiones como STATELESS
         // Esto es crucial para el uso de JWT, ya que no se almacenan estados de sesión
         // en el servidor
